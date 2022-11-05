072d7bbe1e55153aff8e61c7d73522788b7fb49b
Евгений Сишненко <sishnenko86@mail.ru>
2022-11-05 18:13:27 +0300

    002-TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 interface Book {
-  id: "string",
-  title: "string",
-  description: "string",
-  authors: "string",
-  favorite: "string",
-  fileCover: "string",
-  fileName: "string"
+  id: string,
+  title: string,
+  description: string,
+  authors: string,
+  favorite: string,
+  fileCover: string,
+  fileName: string
 }
 
 class BooksRepository  {
-  createBook(book: Book){}
-  getBook(id: number){}
+  createBook(book: Omit<Book, "id">){}
+  getBook(id: string){}
   getBooks(){}
-  updateBook(id: number){}
-  deleteBook(id: number){}
+  updateBook(id: string){}
+  deleteBook(id: string){}
 }
